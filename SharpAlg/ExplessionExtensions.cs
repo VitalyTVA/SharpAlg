264b12ec2283fab15fa3da567bcfd319015cc782
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-06-13 01:53:29 +0400

    Create ExpressionExtenstions

--- SharpAlg/ExplessionExtensions.cs
+++ SharpAlg/ExplessionExtensions.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SharpAlg
-{
-    public class Class1
-    {
+namespace SharpAlg {
+    public class ExplessionExtensions {
     }
 }
---
+++ SharpAlg/Tests/DiffTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SharpAlg.Tests {
+    [TestFixture]
+    public class DiffTests {
+        [Test]
+        public void PolinomDiff() {
+
+        }
+    }
+}
