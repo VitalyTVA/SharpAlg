7b255daa08746539342749e0a5c33f40180fcc92
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-06-18 23:14:36 +0400

    Taste tests

---
+++ SharpAlg/Tests/ParserTests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System.Linq.Expressions;
+using SharpAlg;
+using SharpAlg.Native;
+using SharpKit.JavaScript;
+using Taste;
+using System.IO;
+using System.Text;
+
+namespace SharpAlg.Tests {
+    [TestFixture]
+    public class ParserTests {
+        const string program = @"
+// This is a test program which can be compiled by the Taste-compiler.
+// It reads a sequence of numbers and computes the sum of all integers 
+// up to these numbers.
+
+program Test {
+	int i;
+	
+	void Foo() {
+		int a, b, max;
+		read a; read b;
+        bool x;
+		if (a > b) max = a; else max = b;
+		write max;
+	}
+
+	void SumUp() {
+		int sum;
+		sum = 0;
+		while (i > 0) { sum = sum + i; i = i - 1; }
+		write sum;
+	}
+
+	void Main() {
+		read i;
+		while (i > 0) {
+			SumUp();
+			read i;
+		}
+	}
+}";
+        [Test]
+        public void MainTest() {
+            Scanner scanner = new Scanner(new MemoryStream(Encoding.ASCII.GetBytes(program)));
+            Parser parser = new Parser(scanner);
+            parser.tab = new SymbolTable(parser);
+            parser.gen = new CodeGenerator();
+            parser.Parse();
+            Assert.AreEqual(0, parser.errors.count);
+            parser.gen.Decode();
+            string result = parser.gen.Interpret(new MemoryStream(Encoding.ASCII.GetBytes("13 5 120 0")));
+            Assert.AreEqual("91 15 7260 ", result);
+
+        }
+
+    }
+}
