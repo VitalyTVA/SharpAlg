324c434700bd61a21a0a314354cead84021e4f47
Vitaly Tolstikov <vitalytva@yandex.ru>
2013-08-14 01:18:09 +0400

    Include Core files

---
+++ SharpAlg.Core/Compatibility/JsCompatibility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharpKit.JavaScript {
+    public enum JsMode { None, Prototype, Clr }
+    public class JsTypeAttribute : Attribute {
+        public JsTypeAttribute(JsMode mode) {
+        }
+        public string Filename { get; set; }
+    }
+    public class JsMethodAttribute : Attribute {
+        public JsMethodAttribute() {
+        }
+        public string Code { get; set; }
+    }
+}
