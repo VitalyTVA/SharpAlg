c640a3a5fb93de4721dcbd68ae3aa14ebaf152ae
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-05-26 22:51:12 +0400

    t

--- SharpAlg.Geo/Tests/ProblemsTests.cs
+++ SharpAlg.Geo/Tests/ProblemsTests.cs
@@ -6,24 +6,11 @@ using RealPoint = System.Windows.Point;
 namespace SharpAlg.Geo.Tests {
     [TestFixture]
     public class ProblemsTests {
+        #region middle
         [Test, Explicit]
         public void MiddleOfLineSegment_Maple() {
             var res = GetMiddleOfLineSegmentZeroAssertion(Point.FromName('X'), Point.FromName('Y'));
             var mappleCommand = string.Format("simplify({0}); simplify({1});", res.X.Print(), res.Y.Print());
-            //Clipboard.SetText(mappleCommand);
-        }
-        [Test, Explicit]
-        public void AngleBisection_Maple() {
-            var res = GetAngleBisectionZeroAssertion(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
-            //var res = GetAngleBisectionZeroAssertion(new Point(Expr.Zero, Expr.Zero), Point.FromName('B'), Point.FromName('C'));
-            var mappleCommand = string.Format("simplify({0});", res.Print());
-            //Clipboard.SetText(mappleCommand);
-        }
-        [Test, Explicit]
-        public void Perpendicular_Maple() {
-            var res = GetPerpendocularZeroAssertion(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
-            //var res = GetPerpendocularZeroAssertion(new Point(Expr.Zero, Expr.Zero), new Point(Expr.MinusOne, Expr.Zero), new Point(3d.AsConst(), 4d.AsConst()));
-            var mappleCommand = string.Format("simplify({0});", res.Print());
             //Clipboard.SetText(mappleCommand);
         }
         Point GetMiddleOfLineSegmentZeroAssertion(Point p1, Point p2) {
@@ -40,6 +27,16 @@ namespace SharpAlg.Geo.Tests {
             var expected = ExprHelper.Middle(p1, p2);
             return l1_l2.Offset(expected.Invert());
 
+        } 
+        #endregion
+
+        #region angle bisector
+        [Test, Explicit]
+        public void AngleBisection_Maple() {
+            var res = GetAngleBisectionZeroAssertion(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
+            //var res = GetAngleBisectionZeroAssertion(new Point(Expr.Zero, Expr.Zero), Point.FromName('B'), Point.FromName('C'));
+            var mappleCommand = string.Format("simplify({0});", res.Print());
+            //Clipboard.SetText(mappleCommand);
         }
         Expr GetAngleBisectionZeroAssertion(Point A, Point B, Point C) {
             var l1 = Line.FromPoints(A, B);
@@ -52,6 +49,16 @@ namespace SharpAlg.Geo.Tests {
             var bisectrissa = Line.FromPoints(A, middle);
 
             return Expr.Add(LinesOperations.TangentBetween(l1, bisectrissa), LinesOperations.TangentBetween(l2, bisectrissa));
+        } 
+        #endregion
+
+        #region perpendicular
+        [Test, Explicit]
+        public void Perpendicular_Maple() {
+            var res = GetPerpendocularZeroAssertion(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
+            //var res = GetPerpendocularZeroAssertion(new Point(Expr.Zero, Expr.Zero), new Point(Expr.MinusOne, Expr.Zero), new Point(3d.AsConst(), 4d.AsConst()));
+            var mappleCommand = string.Format("simplify({0});", res.Print());
+            //Clipboard.SetText(mappleCommand);
         }
         Expr GetPerpendocularZeroAssertion(Point A, Point B, Point C) {
             var l1 = Line.FromPoints(A, B);
@@ -67,6 +74,32 @@ namespace SharpAlg.Geo.Tests {
             var cotangent = LinesOperations.CotangentBetween(l1, l3);
             return cotangent;
 
+        } 
+        #endregion
+
+        #region perpendicular
+        [Test, Explicit]
+        public void Perpendicular2_Maple() {
+            var res = GetPerpendocularZeroAssertion2(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
+            //var res = GetPerpendocularZeroAssertion(new Point(Expr.Zero, Expr.Zero), new Point(Expr.MinusOne, Expr.Zero), new Point(3d.AsConst(), 4d.AsConst()));
+            var mappleCommand = string.Format("simplify({0});", res.Print());
+            //Clipboard.SetText(mappleCommand);
         }
+        Expr GetPerpendocularZeroAssertion2(Point A, Point B, Point C) {
+            var l1 = Line.FromPoints(A, B);
+
+            var c = Circle.FromPoints(C, A);
+
+            var D = l1.Intersect(c).Item1; //????
+            var l2 = Line.FromPoints(C, D);
+            var E = l2.Intersect(c).Item2; //????1
+
+            var l3 = Line.FromPoints(E, A);
+
+            var cotangent = LinesOperations.CotangentBetween(l1, l3);
+            return cotangent;
+
+        }
+        #endregion
     }
 }
